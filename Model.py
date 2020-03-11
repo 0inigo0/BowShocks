876c0111714759a42c626afce8ff8ba65c8d7f75
kandeld <dnshkandel@gmail.com>
2020-03-10 20:44:31 -0700

    Update Model.py

--- Model.py
+++ Model.py
@@ -66,7 +66,6 @@ model.add(layer.MaxPooling2D(pool_size = (2,2)))
 model.add(layer.Conv2D(128, kernel_size = (3,3), activation = 'relu'))
 model.add(layer.Conv2D(128, kernel_size = (3,3), activation = 'relu')) #it knows what input shape is
 model.add(layer.Conv2D(128, kernel_size = (3,3), activation = 'relu'))
-model.add(layer.Conv2D(320, kernel_size = (3,3), activation = 'relu'))
 model.add(layer.MaxPooling2D(pool_size = (2,2)))
 
 model.add(layer.Flatten())
